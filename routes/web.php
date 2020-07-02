428180064c1a16bff1c4b0f9b731b0358fd1f01a
Trevor143 <mukws18@gmail.com>
2020-07-02 17:16:15 +0300

    update route to add author

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 
 Route::post('/books', 'BooksController@store');
 Route::patch('/books/{id}', 'BooksController@update');
+Route::delete('/books/{id}', 'BooksController@destroy');
+
+Route::resource('author', 'AuthorController');
